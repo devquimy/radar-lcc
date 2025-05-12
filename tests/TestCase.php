39f4e300ba6b2bdadd28835b103786168e3d8be6
LukeRocha <lucas.ar93@gmail.com>
2025-05-12 19:14:02 -0300

    adds test case file

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,26 @@
 
 namespace Tests;
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Database\Eloquent\Model;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    use CreatesApplication;
+    use RefreshDatabase;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        // Agora só precisa desabilitar mass assignment:
+        Model::unguard();
+    }
+
+    protected function tearDown(): void
+    {
+        Model::reguard();
+        parent::tearDown();
+    }
 }
